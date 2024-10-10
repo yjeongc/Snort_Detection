30a131c16d867283577b672733b721138215b64b
yjeongc <yjeong34@duksung.ac.kr>
2024-10-11 01:02:36 +0900

    Fix: log_to_db.py Error

--- BE/detection/models.py
+++ BE/detection/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 class SnortAlert(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
-    alert_message = models.TextField()
-    source_ip = models.GenericIPAddressField()
-    destination_ip = models.GenericIPAddressField()
+    alert_message = models.CharField(max_length=255)
+    source_ip = models.CharField(max_length=15)
+    destination_ip = models.CharField(max_length=15)
     protocol = models.CharField(max_length=10)
 
     def __str__(self):
         return self.alert_message
+
